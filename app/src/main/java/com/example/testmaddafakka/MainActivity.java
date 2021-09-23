7e87c26efe326ad8f1af6c538beb75da8fb534f5
Mange99 <lord.tobbe@gmail.com>
2021-09-23 13:37:03 +0200

    test git magnus

--- app/src/main/java/com/example/testmaddafakka/MainActivity.java
+++ app/src/main/java/com/example/testmaddafakka/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StartPage startPage = new StartPage();
         final ImageView backBtn = findViewById(R.id.backArrow);
-
+        //Test github
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
         fragmentTransaction.add(R.id.fragmentContainer, startPage);
