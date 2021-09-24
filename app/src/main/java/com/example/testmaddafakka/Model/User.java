02f8b3e6cd877acdbc2e38855a087b68ade61611
Ali Shereef <aliasgar.shereef@hotmail.com>
2021-09-24 10:07:08 +0200

    First User class was for try git

--- app/src/main/java/com/example/testmaddafakka/Model/User.java
+++ app/src/main/java/com/example/testmaddafakka/Model/User.java
@@ -1,4 +1,42 @@
 package com.example.testmaddafakka.Model;
 
 public class User {
+
+    private String name;
+    private String password;
+    private watchList watchList;
+    private Settings Settings ;
+    private Preferences Preferences;
+
+
+
+
+    public User (String name, String password, List watchList, Settings Settings, Preferences preferences) {
+        this.name = name;
+        this.password = password;
+        this.watchList = watchList;
+        this.Settings = Settings;
+        this.Preferences = Preferences;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public watchList getWatchList() {
+        return watchList;
+    }
+
+    public Settings getSettings() {
+        return Settings;
+    }
+
+    public Preferences getPreferences() {
+        return Preferences;
+    }
 }
