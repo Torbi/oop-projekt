dd87aa91d22b2ecc2715044225d671b70636e218
sundstromalbin <albinsundstrom@hotmail.com>
2021-09-28 11:38:35 +0200

    GUI for preferences page

--- app/src/main/java/com/example/testmaddafakka/View/PreferencesView.java
+++ app/src/main/java/com/example/testmaddafakka/View/PreferencesView.java
@@ -3,10 +3,12 @@ package com.example.testmaddafakka.View;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.testmaddafakka.Model.Movie;
 import com.example.testmaddafakka.R;
@@ -16,13 +18,53 @@ import java.util.List;
 
 public class PreferencesView extends Fragment implements ViewListener {
 
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preferences_view, container, false);
+        view = inflater.inflate(R.layout.fragment_preferences_view, container, false);
+
+        Button addGenresBtn = (Button) view.findViewById(R.id.addGenresBtn);
+        Button addActorsBtn = (Button) view.findViewById(R.id.addActorsBtn);
+        Button addDirectorsBtn = (Button) view.findViewById(R.id.addDirectorsBtn);
+
+        addGenresBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragmentContainer, new Genres());
+                fr.addToBackStack(null);
+                fr.commit();
+            }
+        });
+
+        addActorsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragmentContainer, new Actors());
+                fr.addToBackStack(null);
+                fr.commit();
+            }
+        });
+
+        addDirectorsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragmentContainer, new Directors());
+                fr.addToBackStack(null);
+                fr.commit();
+            }
+        });
+
+        return view;
     }
 
     public void update(List<Movie> list){
     }
+
+
 }
