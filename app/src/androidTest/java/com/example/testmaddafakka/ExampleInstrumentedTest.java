16182d5fb04e1a14b09c51500abd279e585d9da6
Baller321 <74151862+Baller321@users.noreply.github.com>
2021-10-06 09:27:05 +0200

    Wrote some tests, unfortunately not able to run them.the console says: Manifest merger failed with multiple errors, see logs.

--- app/src/androidTest/java/com/example/testmaddafakka/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/testmaddafakka/ExampleInstrumentedTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.example.testmaddafakka.repository.FilmsterRepository;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -17,10 +19,65 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    FilmsterRepository repo;git
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
+
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        repo = FilmsterRepository.getInstance(appContext);
         assertEquals("com.example.testmaddafakka", appContext.getPackageName());
     }
+
+    @Test
+    public void loadMoviesTest() {
+        // Context of the app under test.
+
+        repo.loadMovies();
+
+        assertEquals(250, repo.getTop250Movies().getValue().size());
+    }
+
+    @Test
+    public void getCurrentMovieTest() {
+        // Context of the app under test.
+
+        repo.getCurrentMovie();
+
+        assertEquals(  "Inception",repo.getCurrentMovie().getValue().getTitle());
+    }
+
+
+
+    @Test
+    public void addLikedMovieTest() {
+        // Context of the app under test.
+
+
+        repo.addLikedMovie(repo.getTop250Movies().getValue().get(3));
+        repo.addLikedMovie(repo.getTop250Movies().getValue().get(5));
+        repo.addLikedMovie(repo.getTop250Movies().getValue().get(10));
+
+        assertEquals(  3,repo.getLikedMovies().size());
+    }
+
+    @Test
+    public void addDislikedMovieTest() {
+        // Context of the app under test.
+
+
+        repo.addDislikedMovie(repo.getTop250Movies().getValue().get(3));
+        repo.addDislikedMovie(repo.getTop250Movies().getValue().get(5));
+        repo.addDislikedMovie(repo.getTop250Movies().getValue().get(10));
+
+        assertEquals(  3,repo.getDislikedMovies().size());
+    }
+
+
+
+
 }
