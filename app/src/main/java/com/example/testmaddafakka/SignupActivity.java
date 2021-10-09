800b7eafe78ffab17c95c5027729578de8543b2d
Baller321 <74151862+Baller321@users.noreply.github.com>
2021-10-09 16:38:51 +0200

    GUI for inlog in progress

---
+++ app/src/main/java/com/example/testmaddafakka/SignupActivity.java
@@ -0,0 +1,28 @@
+package com.example.testmaddafakka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Button backToLogin = findViewById(R.id.backToLogin);
+
+
+        backToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in=new Intent(SignupActivity.this,MainActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+}
