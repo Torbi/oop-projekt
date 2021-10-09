d53efd02f57bc39b242813fea616a9c156487d2e
Baller321 <74151862+Baller321@users.noreply.github.com>
2021-10-09 16:38:51 +0200

    GUI for inlog in progress

--- app/src/main/java/com/example/testmaddafakka/MainActivity.java
+++ app/src/main/java/com/example/testmaddafakka/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.testmaddafakka;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.testmaddafakka.view.MainView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.fragmentContainer, mainView);
         fragmentTransaction.commit();
+
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in = new Intent(MainActivity.this,SignupActivity.class);
+
+                startActivity(in);
+            }
+        });
     }
 }
 
