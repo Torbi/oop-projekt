e0a67b9aa3b4c3ff98c2692ce109bb3aa836bb88
sundstromalbin <albinsundstrom@hotmail.com>
2021-10-19 17:30:03 +0200

    collects a list of movies but some problem with parsing

--- app/src/main/java/com/filmster/application/api/parse_buildrequest_strategies/IMDbNameBuildRequestStrategy.java
+++ app/src/main/java/com/filmster/application/api/parse_buildrequest_strategies/IMDbNameBuildRequestStrategy.java
@@ -3,19 +3,22 @@ package com.filmster.application.api.parse_buildrequest_strategies;
 import com.filmster.application.api.URL;
 
 /**
- *Uses the IMDbNameSearch option to get a list of actors/directors from IMDb
- *The request has to be a name in format url/SearchName/key/name
+ *Uses the IMDbName option to get a list of movies with one specific actor or director
+ *The request has to be a name in format url/Name/key/id
  *@author Albin Sundström
  */
 
-public class IMDbNameBuildRequestStrategy implements IBuildRequestStrategy{
+
+public class IMDbNameBuildRequestStrategy implements IBuildRequestStrategy {
+
     /**
-     * Build a request to get a list of actors/directors.
+     * Build a request to get a list of movies with one specific actor or director.
      * @param request - a name of an actor or director
-     * @return A correct request in the form of a String, url/SearchName/key/name
+     * @return A correct request in the form of a String, url/Name/key/id
+     * "" needs to be removed to make a correct request.
      */
     @Override
     public String buildRequest(String request) {
-        return URL.IMDB_URL.getValue() + "SearchName" + URL.IMDB_KEY.getValue() + "/" + request;
+        return URL.IMDB_URL.getValue() + "Name" + URL.IMDB_KEY.getValue() + "/" + request.replace("\"", "");
     }
 }
