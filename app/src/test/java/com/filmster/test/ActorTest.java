a45ac244c22f142de86e6c1f24e33161b4f1d58d
Mange99 <you@example.com>
2021-10-24 19:53:40 +0200

    actor test

---
+++ app/src/test/java/com/filmster/test/ActorTest.java
@@ -0,0 +1,33 @@
+package com.filmster.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import androidx.test.annotation.Beta;
+
+import com.filmster.application.model.Actor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActorTest {
+    private Actor actor;
+
+    @Before
+    public void before(){
+        actor = new Actor("1337", "Bruce", null);
+    }
+    @Test
+    public void getID() {
+        assertEquals("1337", actor.getID());
+    }
+
+    @Test
+    public void getImage() {
+        assertNull(actor.getImage());
+    }
+    @Test
+    public void getName() {
+        assertEquals("Bruce", actor.getName());
+    }
+}
